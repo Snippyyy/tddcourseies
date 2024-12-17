98ee81b27347094d46c0c9154f66d62a0efea8b7
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2024-12-17 16:09:05 +0100

    Fin Paso 14

--- tests/Feature/Pages/PageDashboardTest.php
+++ tests/Feature/Pages/PageDashboardTest.php
@@ -4,6 +4,7 @@ use App\Models\Course;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Carbon;
 use function Pest\Laravel\get;
 
 uses(RefreshDatabase::class);
@@ -36,24 +37,46 @@ it('lists purchased courses', function () {
 
 it('does not list other courses', function () {
     // Arrange
+    $user = User::factory()->create();
+    $course = Course::factory()->create();
 
-    // Act
+    // Act & Assert
+    $this->actingAs($user);
+    get(route('dashboard'))
+        ->assertOk()
+        ->assertDontSeeText($course->title);
 
-    // Assert
 });
 
 it('shows latest purchased course first', function () {
     // Arrange
+    $user = User::factory()->create();
+    $firstPurchasedCourse = Course::factory()->create();
+    $secondPurchasedCourse = Course::factory()->create();
 
-    // Act
+    $user->courses()->attach($firstPurchasedCourse, ['created_at' => Carbon::yesterday()]);
+    $user->courses()->attach($secondPurchasedCourse, ['created_at' => Carbon::now()]);
 
-    // Assert
+    // Act & Assert
+    $this->actingAs($user);
+    get(route('dashboard'))
+        ->assertOk()
+        ->assertSeeTextInOrder([
+            $secondPurchasedCourse->title,
+            $firstPurchasedCourse->title,
+        ]);
 });
 
 it('includes link to product videos', function () {
     // Arrange
+    $user = User::factory()
+        ->has(Course::factory())
+        ->create();
 
-    // Act
-
-    // Assert
+    // Act & Assert
+    $this->actingAs($user);
+    get(route('dashboard'))
+        ->assertOk()
+        ->assertSeeText('Watch videos')
+        ->assertSee(route('page.course-videos', Course::first()));
 });
